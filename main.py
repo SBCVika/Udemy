aed3e55897e50e615e435d9d6738e01ea0e7f239
taranyuk.viktoria@gmail.com <taranyuk.viktoria@gmail.com>
2024-02-24 20:00:57 +0100

    something new

---
+++ main.py
@@ -0,0 +1,10 @@
+def myfunc():
+    print("Name?")
+    k = input()
+    if k == "Vika":
+        print('hi Mom')
+    elif k == "Sonia":
+        print("Hi Sonia")
+    else:
+        print("Who are you?")
+myfunc()
