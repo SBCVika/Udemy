d8b059124cb71ff44f67c622e73233773716a4af
taranyuk.viktoria@gmail.com <taranyuk.viktoria@gmail.com>
2024-02-26 17:22:27 +0100

    todo

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-def myfunc():
-    print("Name?")
-    k = input()
-    if k == "Vika":
-        print('hi Mom')
-    elif k == "Sonia":
-        print("Hi Sonia")
-    else:
-        print("Who are you?")
-myfunc()
+todos = []
+
+while True:
+    user_action = input("Type add, show, or exit:")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            for item in todos:
+                print(item)
+        case 'exit':
+            break
+
+print("Bye!")
+
